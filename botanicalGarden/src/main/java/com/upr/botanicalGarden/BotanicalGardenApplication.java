4deb94e734bccd0010abf0cdbb0e87396b604719
Marcin Krykowski <Marcin.Krykowski@gmail.com>
2020-03-29 13:23:19 +0100

    initial commit

---
+++ botanicalGarden/src/main/java/com/upr/botanicalGarden/BotanicalGardenApplication.java
@@ -0,0 +1,13 @@
+package com.upr.botanicalGarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotanicalGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotanicalGardenApplication.class, args);
+	}
+
+}
---
+++ botanicalGarden/src/test/java/com/upr/botanicalGarden/BotanicalGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upr.botanicalGarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotanicalGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
